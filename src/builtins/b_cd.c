3c5d81fa59980e4e64bed0ba7606ca2a166ace06
Igor' Nowikow <tkleiner@ga-n3.msk.21-school.ru>
2020-10-20 21:27:51 +0300

    cd done

--- src/builtins/b_cd.c
+++ src/builtins/b_cd.c
@@ -7,34 +7,17 @@
 //N_PATH - аргумент
 //скорее всего неправильный
 
-int		ft_prep_step(char *c_path, char *n_path, int ind)
+char		*b_cd(char *c_path, const char *n_path)
 {
-	if (n_path[0] == '.')
+	int		ret;
+
+	ret = chdir(n_path);
+	if (ret != -1);
 	{
-		if (n_path[1] == '.' && n_path[2] == '/')
-		{
-			rm_folder(c_path);
-			ind = ft_dellchar(n_path, "123");
-		}
-		else if (n_path[1] == '/')
-			ind = ft_dellchar(n_path, "12");
-		return;
+		ft_bzero(c_path, sizeof(char) * PATH_MAX);
+		getcwd(c_path, PATH_MAX);
 	}
-	ind = add_folder(c_path, n_path);
-}
-
-char	*b_cd(char *c_path, const char *n_path)
-{
-	int		dot_shift;
-	char	*buf_path[2];
-	size_t	ind;
-
-	buf_path[0] = ft_strdup(c_path);
-	buf_path[1] = ft_strdup(n_path);
-	dot_shift = 0;
-	ind = 0;
-	while (n_path[ind])
-		ind += ft_prep_step(buf_path[0], buf_path[1], ind);
-	//MY CODE HERE
+	// else
+		// ERROR
 	return (n_path);
 }
--- src/builtins/b_echo.c
+++ src/builtins/b_echo.c
@@ -9,7 +9,7 @@ ssize_t	b_echo(char *str, int fd, int flag)
 	n_ret = 0;
 	m_ret = write(fd, str, ft_strlen(str));
 	//IF m_ret == -1 -> ERROR
-	if (flag)
+	if (!flag)
 		n_ret = write(fd, "\n", 1);
 	//IF n_ret == -1 -> ERROR
 	return (m_ret + n_ret);
