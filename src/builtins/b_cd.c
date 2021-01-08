30a85e12a9ae3facb8d528c9026935786ca139e9
Enderdroid <igor.novikov6@gmail.com>
2021-01-08 14:59:04 +0300

    added pwd do cd

--- src/builtins/b_cd.c
+++ src/builtins/b_cd.c
@@ -56,6 +56,8 @@ static int	env_arg(char *key, t_exec *exec)
 		ret = chdir(env->value);
 	if (ret == -1)
 		cd_errno(exec, env->value);
+	if (!ft_strcmp(exec->argv[1], "-"))
+		b_pwd(exec);
 	return (0);
 }
 
